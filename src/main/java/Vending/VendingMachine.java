a817021a048478fd58dd7ddca1045d3fa868d418
Victor_506 <viho98001@utb.vaxjo.se>
2019-01-22 13:28:01 +0100

    seond upload

--- src/main/java/Vending/VendingMachine.java
+++ src/main/java/Vending/VendingMachine.java
@@ -3,10 +3,10 @@ package Vending;
 import java.util.List;
 
 public interface VendingMachine
-{
-
-	
-	
-	
-	
+{   
+    public long selectItemAndGetPrice(Item item);
+    public void insertCoin(Coin coin);
+    public List<Coin> refund();
+    public Bucket<Item, List<Coin>> collectItemAndChange();   
+    public void reset();
 }
